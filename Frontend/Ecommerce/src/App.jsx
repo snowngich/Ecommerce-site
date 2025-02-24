d6099278345678bd6bc7dc9414aaa7ff7f18f77b
JOSEPH NJOROGE <snowngich35@gmail.com>
2025-02-24 17:19:18 +0300

    first commit

---
+++ Frontend/Ecommerce/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <header>
+      <div className='logo'>
+        PIGAORDER
+      </div>
+      
+        <a href="">Home</a>
+        <a href="">Products</a>
+        <a href="">Shop here</a>
+        <a href="">Login/Register</a>
+        
+      
+    </header>
+    
+    <section className='hero'>
+      <h1>SHOP WITH US! PIGA IYO ORDER </h1>
+      <h2>Your one-stop shop for the best deals, top-quality products, and hassle-free shopping!</h2>
+      <p>Shop Smart, Save More . Enjoy unbeatable discounts, flash sales, and exclusive offers.</p>
+      <button>🛍️ Start Shopping Now</button>
+      <button>🔥 Explore Hot Deals</button>
+      <button>🚀 Order in Seconds</button>
+     </section> 
+    </div>
+  )
+}
+
+export default App
